9989c0f4623a4c10b38e458165bfcd51ba69d035
Kevin Zuern <kev_zuern@hotmail.com>
2016-02-06 19:19:28 -0500

    Added 'get all registrations' method to data model

--- data_model/model.php
+++ data_model/model.php
@@ -32,6 +32,12 @@ class Database_Reader
 
       return $data->fetch();
    }
+
+   public function get_all_registrations() {
+      $query = "SELECT * FROM `infosheet`";
+      $data = $this->dbh->query($query);
+      return $data->fetch();
+   }
    
    public function valid_user($username, $pw)
    {
